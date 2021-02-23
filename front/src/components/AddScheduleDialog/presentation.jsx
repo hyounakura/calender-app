39a21dcd25ea7dd9fea9d15f806c30190d5ab52a
hyounakura <leopardtender@gmail.com>
2021-02-23 18:37:25 +0900

    add validation

--- front/src/components/AddScheduleDialog/presentation.jsx
+++ front/src/components/AddScheduleDialog/presentation.jsx
@@ -7,7 +7,8 @@ import {
   Button,
   Input,
   Grid,
-  IconButton
+  IconButton,
+  Typography
   } from "@material-ui/core";
 import { LocationOnOutlined, NotesOutlined, AccessTime, Close } from "@material-ui/icons";
 import { DatePicker } from "@material-ui/pickers";
@@ -17,18 +18,22 @@ import * as styles from "./style.css";
 const spacer = { margin: "4px 0" };
 
 const Title = withStyles({
-  root: { marginBottom: 32, fontSize: 22 }
+  root: { fontSize: 22 }
 })(Input);
 
 const AddScheduleDialog = ({
   schedule: {
     form: { title, location, description, date },
-    isDialogOpen
+    isDialogOpen,
+    isStartEdit
   },
   closeDialog,
   setSchedule,
-  saveSchedule
+  saveSchedule,
+  setIsEditStart
 }) => {
+  const isTitleInvalid = !title && isStartEdit;
+
   return (
     <Dialog open={isDialogOpen} onClose={closeDialog} maxWidth="xs" fullWidth>
       <DialogActions>
@@ -40,7 +45,14 @@ const AddScheduleDialog = ({
       </DialogActions>
       <DialogContent>
         <Title autoFocus fullWidth placeholder="タイトルと日時を追加" value={title}
-          onChange={e => setSchedule({ title: e.target.value })} />
+          onChange={e => setSchedule({ title: e.target.value })} onBlur={setIsEditStart} error={isTitleInvalid} />
+        <div className={styles.validation}>
+          {isTitleInvalid && (
+            <Typography variant="caption" component="div" color="error">
+              タイトルは必須です。
+            </Typography>
+          )}
+        </div>
         <Grid container spacing={1} alignItems="center" justify="space-between">
           <Grid item>
             <AccessTime />
